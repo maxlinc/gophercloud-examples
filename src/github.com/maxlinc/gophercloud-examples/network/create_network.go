5b812fc00b3842a20236a54eb97d4875277d68cc
Max Lincoln <max@devopsy.com>
2015-02-13 16:24:40 -0500

    Updated create network test, not quite working

--- src/github.com/maxlinc/gophercloud-examples/network/create_network.go
+++ src/github.com/maxlinc/gophercloud-examples/network/create_network.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "github.com/rackspace/gophercloud"
-  "github.com/rackspace/gophercloud/rackspace"
-  "github.com/rackspace/gophercloud/openstack/networking/v2/networks"
+	"fmt"
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/rackspace"
+	"github.com/rackspace/gophercloud/rackspace/compute/v2/networks"
+	"os"
 )
 
 func main() {
-  ao := gophercloud.AuthOptions{
-    IdentityEndpoint: os.Getenv("OS_AUTH_URL"),
-    Username: os.Getenv("RAX_USERNAME"),
-    APIKey:   os.Getenv("RAX_API_KEY"),
-  }
-  provider, err := rackspace.AuthenticatedClient(ao)
-  if err != nil {
-    panic(err)
-  }
+	ao := gophercloud.AuthOptions{
+		IdentityEndpoint: os.Getenv("OS_AUTH_URL"),
+		Username:         os.Getenv("RAX_USERNAME"),
+		APIKey:           os.Getenv("RAX_API_KEY"),
+	}
+	provider, err := rackspace.AuthenticatedClient(ao)
+	if err != nil {
+		panic(err)
+	}
 
-  client, err := rackspace.NewNetworkV2(provider, gophercloud.EndpointOpts{
-    Name:   "neutron",
-    Region: os.Getenv("RAX_REGION"),
-  })
+	client, err := rackspace.NewNetworkV2(provider, gophercloud.EndpointOpts{
+		Name:   "cloudNetworks",
+		Region: os.Getenv("RAX_REGION"),
+	})
+	if err != nil {
+		panic(err)
+	}
 
-  // We specify a name and that it should forward packets
-  opts := networks.CreateOpts{Name: "main_network", AdminStateUp: networks.Up}
+	// We specify a name and that it should forward packets
+	opts := networks.CreateOpts{
+		Label: "sample_network",
+		CIDR:  "192.0.2.0/24",
+	}
 
-  // Execute the operation and get back a networks.Network struct
-  network, err := networks.Create(client, opts).Extract()
-  fmt.Printf("Created network: %v (%v)", network.Name, network.ID)
+	// Execute the operation and get back a networks.Network struct
+	network, err := networks.Create(client, opts).Extract()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created network: %v (%v)", network.Label, network.CIDR)
 }
